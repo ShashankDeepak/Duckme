11254b812773d92b1daf4bd7e3d8ff21c13d5205
ShashankDeepak <shashankdeepak3@gmail.com>
2022-06-19 21:00:40 +0530

    Added SVGs

--- lib/titlePage.dart
+++ lib/titlePage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class titlePage extends StatelessWidget {
@@ -5,6 +7,22 @@ class titlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Align(
+          alignment: Alignment(0, 0),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xffffaf51), Color(0xffffedd9)],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
