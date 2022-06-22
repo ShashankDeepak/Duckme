d99774e19568f4ea6a1ae86abb9113b99245a1b8
sayanee18 <sayaneesur18@gmail.com>
2022-06-22 15:40:44 +0530

    auth page

--- lib/auth_pages/auth_page.dart
+++ lib/auth_pages/auth_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class authPage extends StatelessWidget {
   const authPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            SvgPicture.asset("assets/Duck logo.svg"),
+            
+            Text("Duckmé",
+            style: GoogleFonts.lilyScriptOne(fontSize: 40),
+              ),
+            ], 
+            ),
+            Text("Bag your dream jobs \n with these amazing \n resumes!",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.lato(fontSize: 25), 
+            ),
+          ],
+        ),
+      );
   }
 }
