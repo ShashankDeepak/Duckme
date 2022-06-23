b793454d4504ed54cd0a86f0d2adec6abea3be15
sayanee18 <sayaneesur18@gmail.com>
2022-06-23 13:18:20 +0530

    Auth Page

--- lib/auth_pages/auth_page.dart
+++ lib/auth_pages/auth_page.dart
@@ -1,31 +1,152 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class authPage extends StatelessWidget {
   const authPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double heightOfDevice = MediaQuery.of(context).size.height;
+    double widthOfDevice = MediaQuery.of(context).size.width;
+
+    double h(double height) {
+      return heightOfDevice * height;
+    }
+
+    double w(double width) {
+      return widthOfDevice * width;
+    }
+
     return Scaffold(
       body: Column(
         children: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.13),
+            ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-            SvgPicture.asset("assets/Duck logo.svg"),
-            
-            Text("Duckmé",
-            style: GoogleFonts.lilyScriptOne(fontSize: 40),
+              SvgPicture.asset("assets/Duck logo.svg"),
+              Text(
+                "Duckmé",
+                style: GoogleFonts.lilyScriptOne(
+                  fontSize: 40,
+                  color: HexColor("FF9900"),
+                ),
               ),
-            ], 
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.05),
             ),
-            Text("Bag your dream jobs \n with these amazing \n resumes!",
+          ),
+          Text(
+            "Bag your dream jobs",
             textAlign: TextAlign.center,
-            style: GoogleFonts.lato(fontSize: 25), 
+            style: GoogleFonts.lato(
+              fontSize: 24,
+              color: HexColor("595959"),
             ),
-          ],
-        ),
-      );
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.01),
+            ),
+          ),
+          Text(
+            "with these amazing",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.lato(
+              fontSize: 24,
+              color: HexColor("595959"),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.01),
+            ),
+          ),
+          Text(
+            "resumes!",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.lato(
+              fontSize: 24,
+              color: HexColor("595959"),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.07),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(60),
+            child: SizedBox(
+              width: w(0.65),
+              height: h(0.065),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: HexColor("FF5D02").withOpacity(0.8),
+                ),
+                child: Text(
+                  "SIGNUP",
+                  style: GoogleFonts.lato(fontSize: 22),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.02),
+            ),
+          ),
+          SizedBox(
+            width: w(0.65),
+            height: h(0.065),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30),
+                  ),
+                ),
+                side: BorderSide(
+                  color: HexColor("FF5D02").withOpacity(0.8),
+                ),
+              ),
+              child: Text(
+                "LOGIN",
+                style: (GoogleFonts.lato(
+                  fontSize: 22,
+                  color: HexColor("FF5D02").withOpacity(0.8),
+                )),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.05),
+            ),
+          ),
+          Text(
+            "This app is powered by Microsoft Student Community, KIIT",
+            style: GoogleFonts.lato(fontSize: 9),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: h(0.015),
+            ),
+          ),
+          SvgPicture.asset("assets/Bottom people.svg"),
+        ],
+      ),
+    );
   }
 }
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,11 +7,13 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseCoreWeb.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
