c1d3bf347a37edf17b1e90d34b03a5870bb53ed9
ShashankDeepak <shashankdeepak3@gmail.com>
2022-07-01 15:10:26 +0530

    home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@
 import 'package:duckme/pages/title_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -68,97 +69,142 @@ class _HomeState extends State<Home> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Container(
-          width: w(1),
-          height: h(1),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              colors: [Color(0xffffaf51), Color(0xffffedd9)],
-            ),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 20.0, top: 30),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Our Picks",
-                    style: GoogleFonts.lato(fontSize: 24),
-                  ),
+        child: Stack(
+          children: [
+            Container(
+              width: w(1),
+              height: h(0.6),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [Color(0xffffaf51), Color(0xffffedd9)],
                 ),
               ),
-              InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    top: h(0.02),
-                    left: w(0.038),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Our Picks",
+                      style: GoogleFonts.lato(fontSize: 30),
+                    ),
                   ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20, left: 10, right: 10),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        width: w(0.27),
-                        height: h(0.2),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Color(0xffd9d9d9),
-                        ),
-                      ),
                       InkWell(
                         onTap: () {},
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 15.0),
-                          child: Container(
-                            width: w(0.3),
-                            height: h(0.25),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Color(0xffd9d9d9),
-                            ),
+                        child: Container(
+                          width: w(0.3),
+                          height: h(0.22),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Color(0xffd9d9d9),
                           ),
                         ),
                       ),
                       InkWell(
                         onTap: () {},
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 15.0),
-                          child: Container(
-                            width: w(0.27),
-                            height: h(0.2),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Color(0xffd9d9d9),
-                            ),
+                        child: Container(
+                          width: w(0.3),
+                          height: h(0.25),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Color(0xffd9d9d9),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: w(0.3),
+                          height: h(0.22),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Color(0xffd9d9d9),
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.0),
-                child: Expanded(
-                  child: Container(
-                    width: w(1),
-                    height: h(1),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(58),
-                        topRight: Radius.circular(58),
-                        bottomLeft: Radius.circular(0),
-                        bottomRight: Radius.circular(0),
+                Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Container(
+                        width: 390,
+                        height: 435,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(58),
+                            topRight: Radius.circular(58),
+                          ),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                                width: 163,
+                                height: 58,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Color(0xffff8e4e),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                      "Fresher",
+                                      style: GoogleFonts.lato(fontSize: 20),
+                                    ),
+                                  ],
+                                )),
+                            Container(
+                                width: 163,
+                                height: 58,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Color(0xffff8e4e),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                            "assets/student1.png",
+                                          ),
+                                          fit: BoxFit.contain,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(
+                                      "Experienced",
+                                      style: GoogleFonts.lato(fontSize: 20),
+                                    ),
+                                  ],
+                                ))
+                          ],
+                        ),
                       ),
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
