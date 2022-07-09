3e88203a4f39efc6fbda6479a0cf4c9904ce9157
ShashankDeepak <shashankdeepak3@gmail.com>
2022-07-09 19:42:58 +0530

    Push

---
+++ lib/Class/user.dart
@@ -0,0 +1,16 @@
+class User {
+  String firstname = "";
+  String lastname = "";
+  String date = "";
+  String email = "";
+  String address = "";
+  int phone = 0123456789;
+
+  String highschool = "";
+  int highSchoolYearJoined = 0;
+  int highSchoolYearLeft = 0;
+  String university = "";
+  // int highSchoolYearJoined = 0;
+  // int highSchoolYearLeft = 0;
+
+}
