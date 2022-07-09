46a6e7aa099ebee07f10592cd6fdb215721683f4
sayanee18 <sayaneesur18@gmail.com>
2022-07-09 21:10:45 +0530

    Merge branch 'main' of https://github.com/ShashankDeepak/Duckme

--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -76,7 +76,88 @@ class _FormPageState extends State<FormPage> {
             },
             body: TabBarView(
               children: [
-                Container(),
+                Container(
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: h(0.025),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  width: w(0.9),
+                                  height: h(0.3),
+                                  decoration: BoxDecoration(
+                                    color: HexColor("F0F0F0"),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: h(0.26),
+                              left: w(0.82),
+                            ),
+                            child: Container(
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: HexColor("000000").withOpacity(0.25),
+                                    blurRadius: 10,
+                                    offset: Offset(0, 4),
+                                  ),
+                                ],
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                  colors: [
+                                    HexColor("EEEEEE"),
+                                    HexColor("FFFFFF")
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: h(0.25),
+                              left: w(0.845),
+                            ),
+                            child: Container(
+                              height: h(0.1),
+                              width: w(0.1),
+                              child: SvgPicture.asset(
+                                  "assets/Icons/Plus icon.svg"),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: h(0.26),
+                              left: w(0.82),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextFormField(
+                        textAlignVertical: TextAlignVertical.top,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'bradjonson119@gmail.com',
+                          labelText: 'First name',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Container(),
                 Container(),
                 Container(),
