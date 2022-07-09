7fa3c5455bab6c1cd295b81db3a429f1c9e24635
sayanee18 <sayaneesur18@gmail.com>
2022-07-09 19:27:45 +0530

    form

--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -28,31 +29,99 @@ class _FormPageState extends State<FormPage> {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: h(0.025),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Container(
+            width: w(1),
+            height: h(1),
+            child: ContainedTabBarView(
+              tabs: [
+                Text('Personal'),
+                Text('Education'),
+              ],
+              views: [
+                Column(
+                  children: [
+                    Stack(
                       children: [
-                        Container(
-                          width: w(0.9),
-                          height: h(0.3),
-                          decoration: BoxDecoration(
-                            color: HexColor("F0F0F0"),
-                            borderRadius: BorderRadius.circular(10),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: h(0.025),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                width: w(0.9),
+                                height: h(0.3),
+                                decoration: BoxDecoration(
+                                  color: HexColor("F0F0F0"),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: h(0.26),
+                            left: w(0.82),
+                          ),
+                          child: Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: HexColor("000000").withOpacity(0.25),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 4),
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(10),
+                              gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  HexColor("EEEEEE"),
+                                  HexColor("FFFFFF")
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: h(0.25),
+                            left: w(0.845),
+                          ),
+                          child: Container(
+                            height: h(0.1),
+                            width: w(0.1),
+                            child:
+                                SvgPicture.asset("assets/Icons/Plus icon.svg"),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: h(0.26),
+                            left: w(0.82),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    TextFormField(
+                      textAlignVertical: TextAlignVertical.top,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'bradjonson119@gmail.com',
+                        labelText: 'First name',
+                      ),
+                    ),
+                  ],
+                ),
+                Container(color: Colors.green),
+              ],
+              onChange: (index) => print(index),
+            ),
           ),
         ),
       ),
