651021914a02b8bd97c1c392800984202c04094a
ShashankDeepak <shashankdeepak3@gmail.com>
2022-07-11 19:23:11 +0530

    Form

--- lib/Class/user.dart
+++ lib/Class/user.dart
@@ -1,4 +1,5 @@
 class User {
+  String uname = "";
   String firstname = "";
   String lastname = "";
   String date = "";
@@ -7,10 +8,68 @@ class User {
   int phone = 0123456789;
 
   String highschool = "";
-  int highSchoolYearJoined = 0;
-  int highSchoolYearLeft = 0;
+  int highSchoolJoined = 0;
+  int highSchoolLeft = 0;
+  double highSchool = 0;
   String university = "";
-  // int highSchoolYearJoined = 0;
-  // int highSchoolYearLeft = 0;
+  int universityJoined = 0;
+  int universityLeft = 0;
+  double universityCGPA = 0;
+  String masters = "";
+  int masterJoined = 0;
+  int masterLeft = 0;
+  double masterCGPA = 0;
 
+  String project1 = "";
+  String project1Detail = "";
+  String project2 = "";
+  String project2Detail = "";
+  String project3 = "";
+  String project3Detail = "";
+
+  String internship1 = "";
+  String internship1Detail = "";
+  int internship1Joined = 0;
+  int internship1Left = 0;
+  String internship2 = "";
+  String internship2Detail = "";
+  int internship2Joined = 0;
+  int internship2Left = 0;
+  String internship3 = "";
+  String internship3Detail = "";
+  int internship3Joined = 0;
+  int internship3Left = 0;
+
+  String currentCompany = "";
+  String currentCompanyDetail = "";
+  String currentCompanyJobRole = "";
+  int currentComanyJoined = 0;
+  int currentComanyLeft = 0;
+  String previousCompany1 = "";
+  String previousCompany1Detail = "";
+  String previousCompany1JobRole = "";
+  int previousCompany1Joined = 0;
+  int previousCompany1Left = 0;
+  String previousCompany2 = "";
+  String previousCompany2Detail = "";
+  String previousCompany2JobRole = "";
+  int previousCompany2Joined = 0;
+  int previousCompany2Left = 0;
+
+  String skill1 = "";
+  int ratingSkill1 = 0;
+  String skill2 = "";
+  int ratingSkill2 = 0;
+  String skill3 = "";
+  int ratingSkill3 = 0;
+  String skill4 = "";
+  int ratingSkill4 = 0;
+  String skill5 = "";
+  int ratingSkill5 = 0;
+  String highlight = "";
+  int leetcode = 0;
+  int codechef = 0;
+
+  String github = "";
+  String linkdin = "";
 }
