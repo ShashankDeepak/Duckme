e4c78c39464d304f61a2b5c6c54203e534956800
sayanee18 <sayaneesur18@gmail.com>
2022-07-15 12:15:41 +0530

    how to use

---
+++ lib/profile pages/how_to_use.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class HowToUse extends StatefulWidget {
+  HowToUse({Key? key}) : super(key: key);
+
+  @override
+  State<HowToUse> createState() => _HowToUseState();
+}
+
+class _HowToUseState extends State<HowToUse> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
